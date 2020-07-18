5859142a7266f85aa86c00c265f27cefe5be867f
AhmedKhalil777 <progeng_Ahmed_Khalil@outlook.com>
2020-07-18 10:24:45 +0200

    Hello MyApp

---
+++ TeraGit.Domain/TeraGit.Domain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeraGit.Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
